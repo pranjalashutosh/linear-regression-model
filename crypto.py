3045ceadbf193dd6720941a5db9363bc03e41c11
Ashutosh Pranjal <pranjalashutosh09@gmail.com>
2025-01-20 19:31:56 -0700

    Still organizing

--- crypto.py
+++
@@ -1,9 +0,0 @@
-#from cryptography.fernet import Fernet
-
-#key = Fernet.generate_key()
-
-#cipher = Fernet(Key)
-
-#def encrypt_text(plain_text):
-#    encrypted_text = cipher.encrypt(plain_text.encode)
-#    return encrypted_text
